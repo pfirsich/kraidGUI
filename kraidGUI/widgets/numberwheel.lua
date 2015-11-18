b15384d88b142ed33e6f0855577e11a5bd7c10f0
joelschum@gmail.com <joelschum@gmail.com>
2015-11-18 16:30:39 +0100

    revamped input filtering (only allow numbers)

--- kraidGUI/widgets/numberwheel.lua
+++ kraidGUI/widgets/numberwheel.lua
@@ -14,11 +14,17 @@ function getModule(gui)
         -- again this object is constructed first, because it can't be with a parent given, because Base.init has to be called first. Also Base.init will
         -- call setValue and therefore set the text in this widget.
         local onChangeLine = function(inputLine, before)
-            if tonumber(inputLine.text) == nil then return true end -- if the current text can not be tonumber-ed, reject changes
-            self:setValue(tonumber(inputLine.text))
+            if tonumber(inputLine.text) ~= nil then self:setValue(tonumber(inputLine.text)) end 
         end
 
-        self.numberInputLine = gui.widgets.LineInput{parent = nil, onChange = onChangeLine}
+        self.numberInputLine = gui.widgets.LineInput{parent = nil, onChange = onChangeLine, onFocusLost = function(line) self:onFocusLost() end,
+            keyPressed = function(line, key, isrepeat)
+                kraid.widgets.LineInput.keyPressed(line, key, isrepeat)
+                if key == "return" then 
+                    self:updateText()
+                    self:focus(nil)
+                end
+            end}
 
         gui.widgets.Base.init(self, params)
         gui.internal.callThemeFunction(self, "init")
@@ -27,12 +33,19 @@ function getModule(gui)
         self.numberInputLine:setParam("text", tostring(self.value))
     end
 
+    function Numberwheel:updateText()
+        self.numberInputLine.text = string.format(self.format, self.value) -- so onChange is not calle
+    end
+
+    function Numberwheel:onFocusLost() 
+        self:updateText()
+    end
+
     function Numberwheel:setValue(value)
         if self.maxValue then value = math.min(self.maxValue, value) end
         if self.minValue then value = math.max(self.minValue, value) end
         if self.onChange then value = self:onChange(value) or value end
         self.value = value
-        self.numberInputLine.text = string.format(self.format, self.value) -- so onChange is not called
     end
 
     Numberwheel.static.setters["value"] = Numberwheel.setValue
